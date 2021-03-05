5919919b1fed36e9ea3d7eb8dbff0c8fdf2e9517
Michael Gearon <mgearon@companieshouse.gov.uk>
2021-03-05 09:48:23 +0000

    Adding lookup

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Update directors details for a company on the register',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+// Sign in
+require('./routes/sign-in.js')(router)
+// Company lookup
+require('./routes/company-lookup.js')(router)
+
 // Add your routes here - above the module.exports line
+router.get('/directors', function (req, res) {
+  // Render the confirm company page
+  res.render('directors', {
+    // To use the company data on that page use the following
+    company: req.session.company,
+    companyIncorp: req.session.companyIncorp,
+    officers: req.session.officers
+  })
+})
 
 module.exports = router
