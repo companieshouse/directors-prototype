09b84d19878e7e31107760e6afdde6c255a3abe4
Michael Gearon <mgearon@companieshouse.gov.uk>
2021-03-05 10:30:21 +0000

    Director removed journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Update directors details for a company on the register',
+  serviceName: 'Update officer details on the register',
 
   // Default port that prototype runs on
   port: '3000',
