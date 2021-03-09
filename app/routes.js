66801d7e4485675f6f7620c6adb33486a4384476
Michael Gearon <mgearon@companieshouse.gov.uk>
2021-03-09 09:01:15 +0000

    Changes

--- app/routes.js
+++ app/routes.js
@@ -25,4 +25,51 @@ router.get('/add/add', function (req, res) {
   })
 })
 
+router.get('/remove/remove', function (req, res) {
+  var request = require('request')
+  var apiKey = process.env.CHS_API_KEY
+  var directorId = req.query.director
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.company-information.service.gov.uk/' + directorId,
+    'headers': {
+      'Authorization': apiKey
+    },
+    'json': true
+  }
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    res.render('remove/remove', {
+    // To use the company data on that page use the following
+      director: response.body
+    })
+  })
+})
+
+router.post('/remove/remove', function (req, res) {
+  res.redirect('../directors')
+})
+
+router.get('/change/change', function (req, res) {
+  var request = require('request')
+  var apiKey = process.env.CHS_API_KEY
+  var directorId = req.query.director
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.company-information.service.gov.uk/' + directorId,
+    'headers': {
+      'Authorization': apiKey
+    },
+    'json': true
+  }
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    res.render('change/change', {
+      // To use the company data on that page use the following
+      director: response.body
+    })
+  })
+  // Render the confirm company page
+})
+
 module.exports = router
