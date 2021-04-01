19cb9984edcdb36e346837eeaee7812a4be3aa41
Jason <jdeakin@companieshouse.gov.uk>
2021-04-01 11:17:26 +0100

    Updating reappoint director journey

--- app/routes.js
+++ app/routes.js
@@ -49,6 +49,31 @@ router.post('/remove/remove', function (req, res) {
   res.redirect('../directors')
 })
 
+router.get('/reappoint/reappoint', function (req, res) {
+  var request = require('request')
+  var apiKey = process.env.CHS_API_KEY
+  var directorId = req.query.director
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.company-information.service.gov.uk/' + directorId,
+    'headers': {
+      'Authorization': apiKey
+    },
+    'json': true
+  }
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    res.render('reappoint/reappoint', {
+    // To use the company data on that page use the following
+      director: response.body
+    })
+  })
+})
+
+router.post('/reappoint/reappoint', function (req, res) {
+  res.redirect('../directors')
+})
+
 router.get('/change/change', function (req, res) {
   var request = require('request')
   var apiKey = process.env.CHS_API_KEY
