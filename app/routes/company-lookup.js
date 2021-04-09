43c05fcbeca64427f041b53715ecf729df66f22a
Jason <jdeakin@companieshouse.gov.uk>
2021-04-09 09:00:46 +0100

    Commenting out dayofincorporation

--- app/routes/company-lookup.js
+++ app/routes/company-lookup.js
@@ -48,7 +48,7 @@ module.exports = function (router) {
         // With the response put that as a session variable so it can be used across all pages
         req.session.company = response.body
         // Format date of incorporation
-        const dayOfIncorporation = req.session.company.date_of_creation.slice(-2)
+        // const dayOfIncorporation = req.session.company.date_of_creation.slice(-2)
         const monthOfIncorporation = req.session.company.date_of_creation.slice(5, 7)
         const yearOfIncorporation = req.session.company.date_of_creation.slice(0, 4)
         req.session.companyIncorp = dayOfIncorporation + ' ' + monthOfIncorporation + ' ' + yearOfIncorporation
