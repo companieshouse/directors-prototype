51e02f0379f6e06d5943fb7f62ed915770171dc3
Michael Gearon <50828707+mgearon-ch@users.noreply.github.com>
2022-02-01 11:02:41 +0000

    Changes

--- app/routes/remove-journey.js
+++ app/routes/remove-journey.js
@@ -2,7 +2,22 @@ module.exports = function (router) {
 
 router.get('/update/remove', function (req, res) {
     req.session.type = req.query.type
-    res.render('update/remove', {
+    var request = require('request')
+    var apiKey = process.env.CHS_API_KEY
+    var directorId = req.query.officer
+    var options = {
+      'method': 'GET',
+      'url': 'https://api.company-information.service.gov.uk/' + directorId,
+      'headers': {
+        'Authorization': apiKey
+      },
+      'json': true
+    }
+    request(options, function (error, response) {
+      if (error) throw new Error(error)
+      res.render('update/remove', {
+        director: response.body
+      })
     })
  })
 router.post('/update/remove', function (req, res) {
