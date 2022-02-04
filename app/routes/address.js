31c93f17dbe03b77a72c383351984be41149593e
Michael Gearon <50828707+mgearon-ch@users.noreply.github.com>
2022-02-04 08:06:14 +0000

    Changes

--- app/routes/address.js
+++ app/routes/address.js
@@ -1,39 +1,49 @@
 module.exports = function (router) {
-router.get('/add/address-lookup-home', function (req, res) {
+
+  //Lookup address page for home address
+router.get('/address-lookup/address-lookup-home', function (req, res) {
     // Render the confirm company page
-    res.render('add/address-lookup-home', {
+    res.render('address-lookup/address-lookup-home', {
+      // To use the company data on that page use the following
+      company: req.session.company
+    })
+  })
+
+    //Lookup address page for the postal address
+  router.get('/address-lookup/address-lookup-postal', function (req, res) {
+    // Render the confirm company page
+    res.render('address-lookup/address-lookup-postal', {
+      // To use the company data on that page use the following
+      company: req.session.company
+    })
+  })
+
+  router.post('/address-lookup/address-lookup-postal', function (req, res) {
+    res.redirect('/address-lookup/home-address')
+  })
+  
+  //Manual address page for the home address
+  router.get('/address-lookup/address-manual-home', function (req, res) {
+    // Render the confirm company page
+    res.render('address-lookup/address-manual-home', {
       // To use the company data on that page use the following
       company: req.session.company
     })
   })
   
-  router.get('/add/address-lookup-postal', function (req, res) {
+  //Manual address page for the postal address
+
+  router.get('/address-lookup/address-manual-postal', function (req, res) {
     // Render the confirm company page
-    res.render('add/address-lookup-postal', {
+    res.render('address-lookup/address-manual-postal', {
       // To use the company data on that page use the following
       company: req.session.company
     })
   })
   
-  router.get('/add/address-manual-home', function (req, res) {
+  router.get('/address-lookup/address-lookup-manual', function (req, res) {
     // Render the confirm company page
-    res.render('add/address-manual-home', {
-      // To use the company data on that page use the following
-      company: req.session.company
-    })
-  })
-  
-  router.get('/add/address-manual-postal', function (req, res) {
-    // Render the confirm company page
-    res.render('add/address-manual-postal', {
-      // To use the company data on that page use the following
-      company: req.session.company
-    })
-  })
-  
-  router.get('/add/address-lookup-manual', function (req, res) {
-    // Render the confirm company page
-    res.render('add/address-lookup-manual', {
+    res.render('address-lookup/address-lookup-manual', {
       // To use the company data on that page use the following
       company: req.session.company
     })
