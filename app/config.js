918e3c494e3a2ef3eed17509e80b6d8d75b54232
shorton-CH <61544357+shorton-CH@users.noreply.github.com>
2022-10-25 15:46:47 +0100

    Content updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Appoint, update and remove a person with significant control (PSC)',
+  serviceName: 'Give notice, update and remove a person with significant control (PSC)',
 
   // Default port that prototype runs on
   port: '3000',
