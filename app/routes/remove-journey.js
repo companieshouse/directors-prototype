b8fea99fd292e9d7eaecd7343409ae2bfb0b19db
Emma <ejames@companieshouse.gov.uk>
2023-03-05 14:42:14 +0000

    Removed DOB on check answers screen for corporate

--- app/routes/remove-journey.js
+++ app/routes/remove-journey.js
@@ -20,6 +20,7 @@ module.exports = function (router) {
       })
     })
   })
+  
   router.post('/update/remove', function (req, res) {
 
 
@@ -28,15 +29,30 @@ module.exports = function (router) {
 
 
 // if the user selects 'Remove corporate director' set the corporate director variable to true and redirect them to the date page
-router.post('/update/remove-corporate', function (req, res) {
-
+router.get('/update/remove-corporate', function (req, res) {
   if (req.session.data['corporateDirector'] != true){
-
     req.session.data['corporateDirector'] = true;
-
   }
-  res.redirect('/update/remove') 
-
+  req.session.type = req.query.type
+  req.session.removename = req.query.directorname
+  var request = require('request')
+  var apiKey = process.env.CHS_API_KEY
+  var directorId = req.query.officer
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.company-information.service.gov.uk/' + directorId,
+    'headers': {
+      'Authorization': apiKey
+    },
+    'json': true
+  }
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    res.render('update/remove', {
+      director: response.body
+    })
   })
+})
+
 
 }
