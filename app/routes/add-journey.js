0cfbf3441be6daf13fe571985889ee565295f1b8
Emma <ejames@companieshouse.gov.uk>
2023-06-05 16:48:02 +0100

    * Re-order flow - Name - Date of appointment - Date of birth *

--- app/routes/add-journey.js
+++ app/routes/add-journey.js
@@ -8,10 +8,31 @@ module.exports = function (router) {
       company: req.session.company
     })
   })
-    
+  
+  //name page goes to date of appointment 
   router.post('/add/name', function (req, res) {
-    res.redirect('/add/date-of-birth')
+
+    res.redirect('/add/date-of-appointment')
+
   })
+
+    // Date of appointment
+    router.get('/add/date-of-appointment', function (req, res) {
+      // Render the confirm company page
+      res.render('add/date-of-appointment', {
+        // To use the company data on that page use the following
+        company: req.session.company
+      })
+    })
+  
+    router.post('/add/date-of-appointment', function (req, res) {
+
+      res.redirect('/add/date-of-birth')
+     
+    }) 
+
+//////////////********     res.redirect('/add/243') */
+
 
   // Date of birth
   router.get('/add/date-of-birth', function (req, res) {
@@ -23,7 +44,30 @@ module.exports = function (router) {
   })
   
   router.post('/add/date-of-birth', function (req, res) {
-    res.redirect('/add/nationality')
+    
+    
+      //if the director details match an existing director, display the duplicate director soft screen
+  
+      if (((req.session.data['firstname'] == 'Andrew')|| (req.session.data['firstname'] == 'andrew')) && ((req.session.data['last-name'] == 'Murray')||(req.session.data['last-name'] == 'murray')) ) {
+        res.redirect('/add/duplicate')
+      }
+      else {
+        res.redirect('/add/nationality') 
+      }
+  
+
+  })
+
+  router.post('/add/duplicate', function (req, res) {
+
+    
+    if (req.session.data['duplicate-director-details'] === 'yes')  {
+      res.redirect('/add/nationality')
+    }
+    else {
+      res.redirect('/Directors')
+    }
+  
   })
 
   // Nationality
@@ -52,39 +96,9 @@ module.exports = function (router) {
     res.redirect('../address-lookup/correspondence-address')
   })
 
-  // Date of appointment
-  router.get('/add/date-of-appointment', function (req, res) {
-    // Render the confirm company page
-    res.render('add/date-of-appointment', {
-      // To use the company data on that page use the following
-      company: req.session.company
-    })
-  })
 
-  router.post('/add/date-of-appointment', function (req, res) {
 
-    //if the director details match an existing director, display the duplicate director soft screen
 
-    if (((req.session.data['firstname'] == 'Andrew')|| (req.session.data['firstname'] == 'andrew')) && ((req.session.data['last-name'] == 'Murray')||(req.session.data['last-name'] == 'murray')) ) {
-      res.redirect('/add/duplicate')
-    }
-    else {
-      res.redirect('/add/243')
-    }
-  
-  }) 
-
-  router.post('/add/duplicate', function (req, res) {
-
-    
-    if (req.session.data['duplicate-director-details'] === 'yes')  {
-      res.redirect('/add/243')
-    }
-    else {
-      res.redirect('/Directors')
-    }
-  
-  })
 
   // 243
   router.get('/add/243', function (req, res) {
